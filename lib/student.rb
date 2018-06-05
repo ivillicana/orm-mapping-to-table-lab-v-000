54868dfe2d4f520d952233a80355c846e53327a1
Isaac Villicana <villicana.i@gmail.com>
2018-06-05 11:45:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,4 +10,15 @@ class Student
     @grade = grade
     @id = id
   end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER,
+      );
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
